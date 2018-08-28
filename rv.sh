8d7a8d4267999608abbe995a14ced794f91137ed
Yiyi Wang <yiyi.wang@runtimeverification.com>
2018-08-28 21:45:41 +0000

    Fix .travis.yml

---
+++ rv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+json_out=`pwd`/errors.json
+report_out=`pwd`/report
+rm -rf $json_out
+rm -rf $report_out
+
+apt install -y autoconf libncurses5-dev libncursesw5-dev
+./autogen.sh
+./configure CC=kcc LD=kcc CFLAGS="-fissue-report=$json_out" --disable-unicode
+make
+timeout 10s ./htop # run the program
+touch $json_out && rv-html-report $json_out -o $report_out
+rv-upload-report $report_out
